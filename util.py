f72282fbea7be79c1b1adccc13158d0372e6c33c
jtayped <jtayped@gmail.com>
2024-04-17 10:06:38 +0200

    code formatting

--- util.py
+++ util.py
@@ -20,12 +20,6 @@ def create_directory(directory):
 def get_random_file(path: str) -> str:
     """
     Get a random file from a directory.
-
-    Args:
-        path (str): Path to the directory.
-
-    Returns:
-        str: Path to the randomly selected file.
     """
     files = os.listdir(path)
     file = random.choice(files)
@@ -35,12 +29,6 @@ def get_random_file(path: str) -> str:
 def get_url_id(url: str) -> str:
     """
     Extract the video ID from a YouTube URL.
-
-    Args:
-        url (str): YouTube video URL.
-
-    Returns:
-        str: Video ID extracted from the URL.
     """
     match = re.search(r"(?:youtube\.com/(?:[-\w]+\?v=|embed/|v/)?)([-\w]+)", url)
     if match:
@@ -58,21 +46,18 @@ def download_youtube_video(
     include_audio: bool = True,
 ) -> str:
     """
-    Downloads a video from a given URL with specified options using yt-dlp library.
+    Downloads a video from a given a URL.
 
     Args:
         url (str): The URL of the video to be downloaded.
+        path (str): Path to the file. This will be joined to the filename
         filename (str): The name of the file, the url ID by default.
-        output_format (str, optional): The output format of the downloaded video. Default is 'mp4'.
+        output_format (str, optional): The file extension of the video.
         max_resolution (int, optional): The maximum resolution of the video. Videos with a resolution equal to or lower than this will be preferred. If set to None, the best available quality will be downloaded. Default is None.
         include_audio (bool, optional): Boolean flag indicating whether to include audio in the downloaded video. Default is True.
 
     Returns:
         file_path (str): Output video path
-
-    Example:
-        url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-        download_video(url, max_resolution=720, include_audio=True, output_format="mp4")
     """
     # Determine the filename if not provided
     if not filename:
@@ -113,55 +98,33 @@ def get_video_duration(video_path: str) -> float:
     """
     Get the duration of a video file.
 
-    Args:
-        video_path (str): Path to the video file.
-
     Returns:
         float: Duration of the video in seconds.
     """
-    # Execute ffprobe command to get video duration
+    cmd = f"ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 {video_path}"
     result = subprocess.run(
-        [
-            "ffprobe",
-            "-v",
-            "error",
-            "-show_entries",
-            "format=duration",
-            "-of",
-            "default=noprint_wrappers=1:nokey=1",
-            video_path,
-        ],
+        cmd,
         stdout=subprocess.PIPE,
         stderr=subprocess.DEVNULL,
     )
+
     # Convert duration to float
     return float(result.stdout)
 
 
-def get_video_dimensions(file_path: str) -> Tuple[int, int]:
+def get_video_dimensions(video_path: str) -> Tuple[int, int]:
     """
     Get the width and height of a video file.
 
     Args:
-        file_path (str): Path to the video file.
+        video_path (str): Path to the video file.
 
     Returns:
         Tuple[int, int]: Width and height of the video.
     """
-    # Execute ffprobe command to get video dimensions
+    cmd = f"ffprobe -v error -select_streams v:0 -show_entries stream=width,height -of csv=s=x:p=0 {video_path}"
     result = subprocess.run(
-        [
-            "ffprobe",
-            "-v",
-            "error",
-            "-select_streams",
-            "v:0",
-            "-show_entries",
-            "stream=width,height",
-            "-of",
-            "csv=s=x:p=0",
-            file_path,
-        ],
+        cmd,
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
         text=True,
